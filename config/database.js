0aec08d3739d116b0199857db266b619d3fb0c37
ARVIND KUMAR <arvindnisha2002@gmail.com>
2024-02-24 14:44:19 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,14 +1,21 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const { MONGODB_URL } = process.env;
+
 
 exports.connect = () => {
-	mongoose
-		.connect(MONGODB_URL,)
-		.then(console.log(`DB Connection Success`))
+	
+
+		mongoose.connect("mongodb+srv://arvindnnic1989602:b8mxguJgLSU8JOZG@cluster0.7cjpaz6.mongodb.net/Koinx_Backend_Assignment", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+			dbName: 'Koinx_Backend_Assignment',
+  retryWrites: true,
+  w: "majority"
+})
+		.then(console.log(`DB ka Connection Success`))
 		.catch((err) => {
-			console.log(`DB Connection Failed`);
+			console.log(`DB ka Connection Failed`);
 			console.log(err);
 			process.exit(1);
 		});
